d4294fd613e0eb311ed6859cf2a2b39a5e307593
HaolinZhong <hz2771@cumc.columbia.edu>
2022-08-17 16:58:35 -0400

    shorten service name

--- src/main/java/hz/spring/brewerygateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/hz/spring/brewerygateway/config/LoadBalancedRoutesConfig.java
@@ -16,14 +16,14 @@ public class LoadBalancedRoutesConfig {
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("lb://brewery-service"))
                 .route(r -> r.path("/api/v1/customers/**")
-                        .uri("lb://brewery-order-service"))
+                        .uri("lb://order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
                         .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
                                         .setFallbackUri("forward:/inventory-failover")
                                         .setRouteId("inv-failover")))
-                        .uri("lb://brewery-inventory-service"))
+                        .uri("lb://inventory-service"))
                 .route(r ->r.path("/inventory-failover/**")
-                        .uri("lb://brewery-inventory-failover"))
+                        .uri("lb://inventory-failover"))
                 .build();
 }
 }
